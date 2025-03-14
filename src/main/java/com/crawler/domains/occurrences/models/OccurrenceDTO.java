bb04a830df3bc0e8569acd9edb4a187c8ea4434b
DomenicoSacco94 <domenico@element.in>
2025-03-14 18:10:20 +0100

    fix the usual boring serialization issues

--- src/main/java/com/crawler/domains/occurrences/models/OccurrenceDTO.java
+++ src/main/java/com/crawler/domains/occurrences/models/OccurrenceDTO.java
@@ -1,11 +1,13 @@
 package com.crawler.domains.occurrences.models;
 
 import com.crawler.domains.regexp.models.Regexp;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OccurrenceDTO {
     private Long id;
     private Regexp regexp;

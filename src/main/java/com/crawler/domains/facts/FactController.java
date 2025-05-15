1592a05b1638a5ef56aed973dc633e111c1dfd68
DomenicoSacco94 <domenico@element.in>
2025-05-15 14:35:38 +0200

    add openAPI docs to the controllers

--- src/main/java/com/crawler/domains/facts/FactController.java
+++ src/main/java/com/crawler/domains/facts/FactController.java
@@ -1,18 +1,23 @@
 package com.crawler.domains.facts;
 
 import com.crawler.domains.facts.models.FactDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1")
 @AllArgsConstructor
+@RequestMapping("/v1")
+@Tag(name = "Facts", description = "Manages the Facts, which are information inferred by the Ollama Model from the Occurences")
+
 public class FactController {
 
     private final FactService factService;
 
+    @Operation(summary = "Retrieve all the facts")
     @GetMapping("/facts")
     public List<FactDTO> getFacts() {
         return factService.getAllFacts();
--- src/main/java/com/crawler/domains/topics/TopicController.java
+++ src/main/java/com/crawler/domains/topics/TopicController.java
@@ -1,17 +1,21 @@
 package com.crawler.domains.topics;
 
 import com.crawler.domains.topics.models.TopicDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1")
 @AllArgsConstructor
+@RequestMapping("/v1")
+@Tag(name = "Topics", description = "Manages the Topics to search with the scanner (and the Regexps they are composed of)")
 public class TopicController {
 
     private final TopicService topicService;
 
+    @Operation(summary = "Create a new Topic")
     @PostMapping("/topics")
     public ResponseEntity<TopicDTO> createTopic(@RequestBody TopicDTO topicDTO) {
         TopicDTO createdTopic = topicService.createTopic(topicDTO);
